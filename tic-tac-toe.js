d72ac78dc37f1da8bef592cd3e68c15a9d033e54
Gabriell Gardner <gabriell.github@gmail.com>
2022-10-10 15:08:32 -0500

    Set div to class square and added DomContentLoaded handler

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,17 @@
-let squares = document.getElementsByClassName('.square');
-
-document.addEventListener('DOMContentLoaded'), () =>
+document.addEventListener('DOMContentLoaded', (event) => 
 {
-    let squares = document.getElementsByClassName('.square');
-}
+
+
+const square1 = document.getElementById('board').getElementsByTagName('div')[0].setAttribute("class", "square");
+const square2 = document.getElementById('board').getElementsByTagName('div')[1].setAttribute("class", "square");
+const square3 = document.getElementById('board').getElementsByTagName('div')[2].setAttribute("class", "square");
+const square4 = document.getElementById('board').getElementsByTagName('div')[3].setAttribute("class", "square");
+const square5 = document.getElementById('board').getElementsByTagName('div')[4].setAttribute("class", "square");
+const square6 = document.getElementById('board').getElementsByTagName('div')[5].setAttribute("class", "square");
+const square7 = document.getElementById('board').getElementsByTagName('div')[6].setAttribute("class", "square");
+const square8 = document.getElementById('board').getElementsByTagName('div')[7].setAttribute("class", "square");
+const square9 = document.getElementById('board').getElementsByTagName('div')[8].setAttribute("class", "square");
+
+
+});
+
