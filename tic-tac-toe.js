3f174aa8604b0e43704d70b5c054bc1b09b5782f
Gabriell Gardner <gabriell.github@gmail.com>
2022-10-11 20:40:00 -0500

    Added X,O functionality and array to keep track of game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,15 +2,59 @@ document.addEventListener('DOMContentLoaded', (event) =>
 {
 
 
-const square1 = document.getElementById('board').getElementsByTagName('div')[0].setAttribute("class", "square");
-const square2 = document.getElementById('board').getElementsByTagName('div')[1].setAttribute("class", "square");
-const square3 = document.getElementById('board').getElementsByTagName('div')[2].setAttribute("class", "square");
-const square4 = document.getElementById('board').getElementsByTagName('div')[3].setAttribute("class", "square");
-const square5 = document.getElementById('board').getElementsByTagName('div')[4].setAttribute("class", "square");
-const square6 = document.getElementById('board').getElementsByTagName('div')[5].setAttribute("class", "square");
-const square7 = document.getElementById('board').getElementsByTagName('div')[6].setAttribute("class", "square");
-const square8 = document.getElementById('board').getElementsByTagName('div')[7].setAttribute("class", "square");
-const square9 = document.getElementById('board').getElementsByTagName('div')[8].setAttribute("class", "square");
+
+var square1 = document.getElementById('board').getElementsByTagName('div')[0].setAttribute("class", "square");
+var square1 = document.getElementById('board').getElementsByTagName('div')[1].setAttribute("class", "square");
+var square1 = document.getElementById('board').getElementsByTagName('div')[2].setAttribute("class", "square");
+var square1 = document.getElementById('board').getElementsByTagName('div')[3].setAttribute("class", "square");
+var square1 = document.getElementById('board').getElementsByTagName('div')[4].setAttribute("class", "square");
+var square1 = document.getElementById('board').getElementsByTagName('div')[5].setAttribute("class", "square");
+var square1 = document.getElementById('board').getElementsByTagName('div')[6].setAttribute("class", "square");
+var square1 = document.getElementById('board').getElementsByTagName('div')[7].setAttribute("class", "square");
+var square1 = document.getElementById('board').getElementsByTagName('div')[8].setAttribute("class", "square");
+
+const squares = document.querySelectorAll('.square');
+
+
+const gameTracker = ["", "", "", "", "", "", "", "", ""];
+const player_X = "X";
+const player_O = "O";
+let currentPlayer = player_X
+
+squares.forEach(function(square)
+{
+    square.addEventListener('click', function()
+    {
+        if(square.innerText.trim() != "") return
+
+        square.innerText = currentPlayer
+
+        if (currentPlayer == "X")
+        {
+            square.setAttribute("class", "square X") && gameTracker.push(scores.indexOf(square))
+        }
+        else
+        {
+            square.setAttribute("class", "square O") && gameTracker.push(scores.indexOf(square))
+        }
+        
+        currentPlayer = currentPlayer == "X" ? "O" : "X"
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+      
+   
+
 
 
 });
